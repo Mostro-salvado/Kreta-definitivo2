7d9d04848c6e9176582dfab3ca6d215494f6570f
Mostrosalvado <diealesal@gmail.com>
2024-01-12 17:26:44 -0300

    Configurar formato de fin de línea

---
+++ src/components/Somos.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function somos() {
+    return (
+        <div>somos</div>
+    )
+}
---
+++ src/routes/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Home from "../containers/Home";
+import Fotografia from "../containers/Fotografia";
+import Audiovisual from "../containers/Audiovisual";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home/>}></Route>
+          <Route exact path="/fotografia" element={<Fotografia/>}></Route>
+          <Route exact path="/audiovisual" element={<Audiovisual/>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
