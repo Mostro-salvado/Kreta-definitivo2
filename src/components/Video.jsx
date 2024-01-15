c58d2c71279a028dc5f7b1547af1522a876db13b
Mostrosalvado <diealesal@gmail.com>
2024-01-15 18:51:56 -0300

    Modularice el video en otro componente

---
+++ src/components/Video.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Video({ videoId }) {
+    return (
+        <div className="video-container">
+        <iframe
+            title="video"
+            width="100%"
+            height="100%"
+            src={`https://www.youtube.com/embed/${videoId}?autoplay=1&controls=0&showinfo=0&loop=1&rel=0`}
+            frameBorder="0"
+            allowFullScreen
+        ></iframe>
+    </div>
+);
+}
+
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import "../assets/css/Home.css";
+import Video from '../components/Video';
 
 export default function Home() {
     const videoId = 'dbcc_1t0in4';
 
     return (
-        <div className="video-container">
-            <iframe
-            title="video"
-            width="100%"
-            height="100%"
-            src={`https://www.youtube.com/embed/${videoId}?autoplay=1&controls=0&showinfo=0&loop=1&rel=0`}
-            frameBorder="0"
-            allowFullScreen
-            ></iframe>
-        </div>
+        <>
+            <div className="video-container">
+                <Video videoId={videoId} />
+            </div>
+        </>
     );
 }
