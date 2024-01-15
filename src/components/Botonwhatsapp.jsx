399bb47974d89239a0fa7fe23248c2b6d98298f0
Mostrosalvado <diealesal@gmail.com>
2024-01-15 18:40:00 -0300

    Agregue el boton flotante de whatsapp

--- src/components/Botonwhatsapp.jsx
+++ src/components/Botonwhatsapp.jsx
@@ -3,6 +3,6 @@ import { Link } from "react-router-dom";
 
 export default function Botonwhatsapp() {
     return (
-        <Link to="/ruta-del-botón" className="boton-flotante">Botón</Link>
+        <Link to="#" className="boton-flotante"></Link>
     )
 }
