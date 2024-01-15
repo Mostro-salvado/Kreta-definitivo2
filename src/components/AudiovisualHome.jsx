92ecc17f68ae60a1d34ba5b312e0a96d9b3c9d8e
Mostrosalvado <diealesal@gmail.com>
2024-01-15 20:56:36 -0300

    Agregue audiovisual en home, falta acomodar el hover de oculto

--- src/components/AudiovisualHome.jsx
+++ src/components/AudiovisualHome.jsx
@@ -1,8 +1,42 @@
-import "../assets/css/AudiovisualHome.css"
+// En tu componente AudiovisualHome.js
+import React, { useEffect, useState } from 'react';
+import '../assets/css/AudiovisualHome.css';
+import { Link } from "react-router-dom";
 
 export default function AudiovisualHome() {
-  return (
-    <div>AudiovisualHome</div>
-  )
-}
+  const [isVisible, setIsVisible] = useState(false);
+  //const [visible , setVisible] = useState(false) aca manejaria el codigo de oculto
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      const threshold = 210; // Puedes ajustar este umbral según tus necesidades
+      console.log("Scroll Y:", scrollY);
+
+      // Cambiar la visibilidad del contenedor basado en la posición del scroll
+      setIsVisible(scrollY > threshold);
+    };
+
+    // Agregar el evento de scroll al montar el componente
+    window.addEventListener('scroll', handleScroll);
+
+    // Eliminar el evento de scroll al desmontar el componente
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []); // La dependencia vacía asegura que el efecto solo se ejecute una vez al montar el componente
+
+  return (
+    <div className={`containerav ${isVisible ? 'visible blur-in-expand' : ''}`}>
+      <div className="audiovisual">
+        <h2>Audiovisual</h2>
+        <div className="oculto">
+          <p className="texto">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat temporibus inventore molestiae esse itaque. Maxime tempore, temporibus dolorum voluptatum, voluptatibus magnam sit necessitatibus beatae, eius ea qui fugit ducimus reiciendis.
+          </p>
+          <Link className="vermas" to="/audiovisual">Ver +</Link>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import "../assets/css/Home.css";
 import Video from '../components/Video';
+import AudiovisualHome from '../components/AudiovisualHome'
 
 export default function Home() {
     const videoId = 'dbcc_1t0in4';
@@ -9,6 +10,9 @@ export default function Home() {
         <>
             <div className="video-container">
                 <Video videoId={videoId} />
+            </div>
+            <div className="audiovisual-home">
+                <AudiovisualHome></AudiovisualHome>
             </div>
         </>
     );
