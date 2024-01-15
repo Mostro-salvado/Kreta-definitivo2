8649d3362d479c1bccbd31f108aec0d6d97d1086
Mostrosalvado <diealesal@gmail.com>
2024-01-15 18:21:57 -0300

    Actualiza la configuración de .gitattributes

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,7 +1,19 @@
-import "../assets/css/Home.css"
+import React from 'react';
+import "../assets/css/Home.css";
 
 export default function Home() {
-    return (
-        <div>Home</div>)
+    const videoId = 'dbcc_1t0in4';
 
+    return (
+        <div className="video-container">
+            <iframe
+            title="video"
+            width="100%"
+            height="100%"
+            src={`https://www.youtube.com/embed/${videoId}?autoplay=1&controls=0&showinfo=0&loop=1&rel=0`}
+            frameBorder="0"
+            allowFullScreen
+            ></iframe>
+        </div>
+    );
 }
