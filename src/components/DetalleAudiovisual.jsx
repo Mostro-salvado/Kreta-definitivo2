c26b98ca7945a412468c305b37864b3a625caf5a
Mostrosalvado <diealesal@gmail.com>
2024-01-25 21:38:19 -0300

    Para revision

--- src/components/DetalleAudiovisual.jsx
+++ src/components/DetalleAudiovisual.jsx
@@ -9,16 +9,19 @@ export default function DetalleAudiovisual({titulo,video,texto}) {
     return (
         <>
             <div className={`container-audiovisual-detalle ${titulo}`}>
-                <div className="titulo-audiovisual-detalle">
-                    {titulo}
-                </div>
-                <div className="video-audiovisual-detalle">
-                    <ReactPlayer url={video} width="750" height="500" controls light=""/>
-                </div>
-                <div className="texto-audiovisual-detalle">
-                    <p>
-                        {texto}
-                    </p>
+                
+                <div className={`wraper-audiovisual ${titulo }`}>
+                    <div className={`video-audiovisual-detalle ${titulo }`}>
+                        <ReactPlayer url={video} width="750" height="500" controls light=""/>
+                    </div>
+                    <div className={`texto-audiovisual-detalle`}>
+                        <div className="titulo-audiovisual-detalle">
+                            {titulo}
+                        </div>
+                        <p>
+                            {texto}
+                        </p>
+                    </div>
                 </div>
             </div>
         </>
--- src/components/ShowAudiovisual.jsx
+++ src/components/ShowAudiovisual.jsx
@@ -7,7 +7,7 @@ export default function ShowAudiovisual() {
         <>
             <div className="oculto">
             <p className="texto">
-                Spots, campañas, coberturas, publicidades y videos institucionales.<br />En Kreta contamos con un equipo audiovisual completo para suplir las necesidades de tu empresa, tu negocio o tu cliente. ¡Mirá nuestro portfolio!
+                Spots, campañas, coberturas, publicidades y videos institucionales.<br />En Kreta contamos con un equipo audiovisual completo para suplir las necesidades de tu empresa, tu negocio o tu cliente.<br /> ¡Mirá nuestro portfolio!
             </p>
                 <Link className="vermas" to="/audiovisual">Ver +</Link>
             </div>
