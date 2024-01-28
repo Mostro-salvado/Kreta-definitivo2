360148e95a6890a8d6c13cd6d7326a2ab3bf9e1e
Mostrosalvado <diealesal@gmail.com>
2024-01-27 23:18:08 -0300

    Saque el autoplay en el video de Home

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -8,7 +8,7 @@ export default function Video({ videoId }) {
             title="video"
             width="100%"
             height="100%"
-            src={`https://www.youtube.com/embed/${videoId}?autoplay=1&controls=0&showinfo=0&loop=1&rel=0`}
+            src={`https://www.youtube.com/embed/${videoId}?&controls=0&showinfo=0&loop=1&rel=0`}
             frameBorder="0"
             allowFullScreen
         ></iframe>
