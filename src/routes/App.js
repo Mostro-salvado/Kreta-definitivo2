849fb3f31963317b824695316367c6767c2628b1
Mostrosalvado <diealesal@gmail.com>
2024-01-29 12:51:31 -0300

    Implementacion de no traduccion

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,13 +1,35 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../containers/Home";
 import Fotografia from "../containers/Fotografia";
 import Audiovisual from "../containers/Audiovisual";
 import Layout from "../components/Layout";
-import Formulario from "../components/Formulario"
+import Formulario from "../components/Formulario";
+
+function bloquearGoogleTranslate() {
+  if (window.google && window.google.translate) {
+    window.google.translate.Element.hideWidget();
+    window.google.translate.TranslateService.disableAutoTranslation();
+  }
+}
+
+function bloquearMenuContextual(e) {
+  e.preventDefault(); // Evita que el menú contextual se abra
+}
 
 function App() {
+  // Usamos useEffect para ejecutar la función de bloqueo cuando el componente se monta
+  useEffect(() => {
+    bloquearGoogleTranslate();
+    // Agregamos el controlador de eventos para el menú contextual
+    document.addEventListener('contextmenu', bloquearMenuContextual);
+    return () => {
+      // Removemos el controlador de eventos cuando el componente se desmonta
+      document.removeEventListener('contextmenu', bloquearMenuContextual);
+    };
+  }, []);
+
   return (
-    
     <>
       <BrowserRouter>
         <Layout>
